d9525e7d525c501ca8c81080d677cf7db90f77a0
chandrabhanu <dcv.kalansooriya@gmail.com>
2023-03-26 04:05:14 +0530

    newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -12,6 +12,24 @@ app.use(express.static(path.join(__dirname, "../public")))
 io.on("connection", socket => {
     console.log("new user connected")
 
+    socket.emit("newMessage", {
+        from: "system",
+        text: "Welcome to the chat app!"
+    })
+
+    socket.broadcast.emit("newMessage", {
+        from: "system",
+        text: "A new user connected!"
+    })
+
+    socket.on("createMessage", message => {
+        console.log("createMessage", message)
+        socket.emit("newMessage", {
+            from: message.from,
+            text: message.text
+        })
+    })
+
     socket.on("disconnect", () => {
         console.log("user disconnected")
     })
